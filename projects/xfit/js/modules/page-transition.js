ee1275716241ad9471f0c820d50d888d60c29902
Timur Syrtsov <telepath2814@gmail.com>
2024-04-14 19:50:08 +0300

    commit

--- projects/xfit/js/modules/page-transition.js
+++ projects/xfit/js/modules/page-transition.js
@@ -11,7 +11,6 @@ function initTransition() {
     if (transition) {
         console.log('transition ' + new Date());
         transition.classList.add('page__transition--in');
-        transition.style.visibility = 'hidden';
     }
 }
 
@@ -32,11 +31,9 @@ function changePage(event) {
     const linkURL = linkNode.attributes.href.value;
 
     transition.classList.replace('page__transition--in', 'page__transition--out');
-    transition.style.visibility = 'visible';
 
     setTimeout(() => {
-        transition.classList.remove('page__transition--out');
-        transition.style.visibility = 'hidden';
+        transition.classList.replace('page__transition--out', 'page__transition--in');
         location = linkURL;
     }, TRANSITION_DURATION_MS);
 }
